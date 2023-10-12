3ccd63accbbc0a03cd3a4fdadd3e35dde1615b86
shrksv <116554502+shrksv@users.noreply.github.com>
2023-10-12 22:33:19 +0300

    laba5

--- src/main/java/flower/store/Flower.java
+++ src/main/java/flower/store/Flower.java
@@ -1,20 +1,26 @@
 package flower.store;
-
-import lombok.Getter;
 import lombok.Setter;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-
-
-@Setter
+@Getter @AllArgsConstructor @ToString @NoArgsConstructor @Setter
 public class Flower {
-    @Getter
+    private FlowerType flowerType;
+    private double price;
     private double sepalLength;
     private FlowerColor color;
-    @Getter
-    private double price;
-    @Getter
-    private FlowerType flowerType;
 
+    public Flower(Flower flower) {
+        this.flowerType = flower.flowerType;
+        this.price = flower.price;
+        this.sepalLength = flower.sepalLength;
+        this.color = flower.color;
+    }
+    public void setPrice(final int newPrice) {
+        this.price = newPrice;
+    }
     public String getColor() {
         return color.toString();
     }
